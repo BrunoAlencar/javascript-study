6495ba0d20e7b37e25ae4cf5bf5e8f004513266b
Bruno Alencar <joaobrunoalencar@gmail.com>
2019-07-23 11:02:41 -0300

    studying arrays

---
+++ array/productList.js
@@ -0,0 +1,24 @@
+const products = [
+    {
+        "name": "product 01",
+        "value": 15
+    },
+    {
+        "name": "product 02",
+        "value": 60
+    },
+    {
+        "name": "product 03",
+        "value": 45
+    },
+    {
+        "name": "product 04",
+        "value": 30,
+        "subarray": [
+            "string1",
+            "string2",
+            "string3",
+            "string4"
+        ]
+    }
+];
